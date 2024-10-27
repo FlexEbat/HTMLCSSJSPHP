e7e59c355d4cb884c10c9079470b0f8a42bf6059
FlexEbat <52438328+FlexEbat@users.noreply.github.com>
2024-10-27 13:14:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<?php
+    echo "Hello world";
+    // echo "X equals $x";
+    #  echo "X equals $x";
+    #http://localhost/site/
+    /* 
+    s9ski mas9ski
+    123
+    123
+    str 83
+    */
+    $mycounter = 5;
+    $mystring = "Hellow";
+    echo "<br>";
+
+
+    echo "<br>";
+    $username = "Adolf Dacler";
+    $current_user = $username;
+    echo $username;
+    echo "<br>";
+ 
+    echo $current_user;
+
+
+    echo "<br>";
+    echo "<br>";
+    $count = 15.4;
+    echo $count;
+    echo "<br>";
+    echo "<br>";
+
+    $oxo = array(array("x","o","x"), 
+                array("o","o","x"), 
+                array("x","o"," "));
+    echo $oxo[0][1];
+    echo "<br>";
+    echo "<br>";
+
+    $myarray = array("One", "Two", "Three");
+    echo $myarray[0];
+
+    echo "<br>";
+    echo "<br>";    
+    echo 6 + 2;
+    echo "<br>";    
+    echo "<br>";    
+    $count = 2;
+    $count += 1;
+    echo $count;
+    echo "<br>";
+    echo "<br>";
+
+
+    $hour = 13;
+    if ($hour > 12 && $hour < 14) {
+        echo "Огромные сиськи";
+    };
+    echo "<br>";
+    echo "<br>";
+    $x = 9 ;
+    #++$x;
+    if (++$x == 10 ) echo"Иди нахуй";
+    echo "<br>";
+    echo "<br>";
+    echo $x;
+    
+    
+    echo "<br>";
+    echo "<br>";    
+    $messages = 105;
+    echo "У вас " . $messages . " сообщений ";
+
+    echo "<br>";    
+    echo "<br>";    
+    $bulletin = "Большие сиськи только тут!!!";
+    $newflash = " Сисяндры тут!!";
+    $bulletin .= $newflash;
+    echo $bulletin;
+    echo "<br>";    
+    echo "<br>";    
+
+
+    $variable = 15;
+    $info = 'Предваряйте имена переменных символом $ как в данном примере:
+    $variable';
+    echo $info;
+    echo "<br>";    
+    $info = "Предваряйте имена переменных символом $ как в данном примере:
+    $variable";
+    echo $info;
+?>
